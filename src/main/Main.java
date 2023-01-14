0d60fe4a6ca132288ee8e91609772cad576b9f4c
Eugene Hong <awidesky@naver.com>
2023-01-14 22:44:01 +0900

    delete unneeded TODO

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,6 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		// https://github.com/awidesky/Youtube-Audio-Auto-Downloader4J/releases/latest/download/YoutubeAudioAutoDownloader.v1.6.5.zip
 
 	}
